6538d4d35d298d57a43f36ee024b15c4788a8725
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2025-07-11 00:12:07 +0600

    added hero section

--- my-portfolio/src/app/home/page.tsx
+++ my-portfolio/src/app/home/page.tsx
@@ -1,13 +1,15 @@
 
 
+import { Hero } from '@/components/Hero/Hero'
 import { Navbar } from '@/components/Navbar/Navbar'
 import React from 'react'
 
 const home = () => {
     return (
-        <main className="flex min-h-screen items-center justify-center">
+        <main className="md:flex min-h-screen items-center justify-center">
             <Navbar />
-
+            <br></br>
+            <Hero />
         </main>
     )
 }
---
+++ my-portfolio/src/components/Hero/Hero.tsx
@@ -0,0 +1,46 @@
+import { ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const data = {
+    paragraph:
+        `I'm an undergraduate Computer Science & Engineering student at American International University in Bangladesh. 
+    I'm a quick learner with a strong interest in web development, driven to solve complex problems and develop 
+    innovative solutions that make a difference. I enjoy the fast-paced nature of web development as a frontend 
+    developer and adapt to emerging trends and industry best practices to produce appealing digital 
+    products that leave a lasting impact on users. My interests also extend to competitive programming, 
+    where I solve complex coding challenges and improve my problem-solving skills. In academics, I have c
+    onsistently achieved top honors, earning a place on the Dean's List for two semesters of my undergraduate studies.`
+};
+
+
+const Hero = () => {
+    return (
+        <section>
+            <div className="container pt-13">
+                <div className="grid items-center gap-8 bg-muted-2 lg:grid-cols-2">
+                    <div className="flex flex-col items-center p-16 text-center lg:items-start lg:text-left">
+                        <h1 className="my-6 text-3xl font-bold text-pretty lg:text-6xl">
+                            Md. Tahsin Hasib
+                        </h1>
+                        <p className="mb-8 max-w-xl text-sm text-justify text-muted-foreground lg:text-md">
+                            {data.paragraph}
+                        </p>
+                        <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
+                            <Button className="cursor-pointer">
+                                Read More
+                                <ArrowRight className="size-4" />
+                            </Button>
+                        </div>
+                    </div>
+                    <img
+                        src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/placeholder-1.svg"
+                        alt="placeholder hero"
+                        className="h-full w-full object-cover"
+                    />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export { Hero };
