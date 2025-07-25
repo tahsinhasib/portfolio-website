82bc3d4a5f51aca8fd5e7d6ee8f9e9999e20b12b
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2025-07-26 01:21:54 +0600

    added vercel analytics

--- my-portfolio/src/app/layout.tsx
+++ my-portfolio/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -36,6 +37,7 @@ export default function RootLayout({
                     disableTransitionOnChange
                 >
                     {children}
+                    <Analytics />
                 </ThemeProvider>
             </body>
         </html>
